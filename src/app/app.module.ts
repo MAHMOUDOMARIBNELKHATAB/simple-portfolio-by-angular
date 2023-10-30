ea00d28e0b2cd9a5ff716c7ffd261f0eea0c4e37
MAHMOUDOMARIBNELKHATAB <momar351@yahoo.com>
2023-10-30 21:40:47 +0200

    simple portfolio by Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderSectionComponent } from './header-section/header-section.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { SkillsComponent } from './skills/skills.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderSectionComponent,
+    AboutMeComponent,
+    SkillsComponent,
+    PortfolioComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/header-section/header-section.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header-section',
+  templateUrl: './header-section.component.html',
+  styleUrls: ['./header-section.component.css']
+})
+export class HeaderSectionComponent {
+  name:string ="Mahmoud Omar Ibn Elkhatab" ;
+  job:string ="Front End Developer" ; 
+}
